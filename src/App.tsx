42e1b93bfccf400fe6e1c799b60231df6cb59fce
m.yoshida <m.yoshida0516@gmail.com>
2025-04-18 17:43:25 +0900

    型定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
+type Todo = {
+  id: number
+  title: string
+  status: 'notStarted' | 'inProgress' | 'done'
+}
+
 function App() {
   return <>first commit</>
 }
